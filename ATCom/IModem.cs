250269a8f90876ebf36cd5eca4684aedc07d2a5e
unknown <bmendoza@MNL1ASPTESTPC.c-cubeservices.com>
2014-11-27 15:17:26 -0500

    Updated Modem Interface.

--- ATCom/IModem.cs
+++ ATCom/IModem.cs
@@ -6,6 +6,26 @@ namespace ATCom
 {
     interface IModem
     {
+        /// <summary>
+        /// Connect to model
+        /// </summary>
+        /// <returns></returns>
         bool Connect();
+        /// <summary>
+        /// Disconnect from model
+        /// </summary>
+        /// <returns></returns>
+        bool Disconnect();
+        /// <summary>
+        /// Call using AT Commands.
+        /// </summary>
+        /// <param name="phoneNumber"></param>
+        void Call(string phoneNumber);
+        /// <summary>
+        /// Send SMS
+        /// </summary>
+        /// <param name="phoneNumber"></param>
+        /// <param name="textMessage"></param>
+        void Send(string phoneNumber, string textMessage);
     }
 }
